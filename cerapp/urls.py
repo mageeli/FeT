551b5412219402a2359435d32538dca83f73a35f
mousa ageeli <mousaageeli@gmail.com>
2020-04-13 15:07:53 +0300

    ss

--- cerapp/urls.py
+++ cerapp/urls.py
@@ -6,7 +6,8 @@ from .views import home, pdf,MyModelView,MyModelDownloadView,WriteView
 urlpatterns = [
     url(r'^$', WriteView.as_view(), name='home'),
     url('m/1', MyModelView.as_view),
-    url(r'^post/(?P<pk>[0-9]+)/$', MyModelView.as_view(), name='post'),
+    url(r'^posts/(?P<pk>[0-9]+)/$', MyModelView.as_view(), name='posts'),
+    url(r'^post/(?P<pk>[0-9]+)/$', MyModelDownloadView.as_view(), name='post'),
     url(r'^write/$', WriteView.as_view(success_url="/posts/"), name='write')
 
 ]
