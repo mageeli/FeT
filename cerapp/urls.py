afed220594b2e45a59d7aa9c6db6fa62e567f11b
mousa ageeli <mousaageeli@gmail.com>
2020-04-13 15:18:09 +0300

    ss

--- cerapp/urls.py
+++ cerapp/urls.py
@@ -2,11 +2,17 @@ from django.conf.urls import url, include
 from django.urls import path
 
 from .views import home, pdf,MyModelView,MyModelDownloadView,WriteView
+from django.conf import settings
+from django.conf.urls.static import static
+
+# if settings.DEBUG:
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     url(r'^$', WriteView.as_view(), name='home'),
     url('m/1', MyModelView.as_view),
-    url(r'^posts/(?P<pk>[0-9]+)/$', MyModelView.as_view(), name='posts'),
+    url(r'^posts/(?P<pk>[0-9]+)/$', MyModelDownloadView.as_view(), name='posts'),
     url(r'^post/(?P<pk>[0-9]+)/$', MyModelDownloadView.as_view(), name='post'),
     url(r'^write/$', WriteView.as_view(success_url="/posts/"), name='write')
 
