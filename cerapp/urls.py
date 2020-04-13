697dc74fe1229e91841f95b8b196783cd3dffcd1
mousa ageeli <mousaageeli@gmail.com>
2020-04-13 15:24:25 +0300

    ss

--- cerapp/urls.py
+++ cerapp/urls.py
@@ -7,12 +7,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', WriteView.as_view(), name='home'),
+    path(r'^$', WriteView.as_view(), name='home'),
     url('m/1', MyModelView.as_view),
-    url(r'^posts/(?P<pk>[0-9]+)/$', MyModelView.as_view(), name='posts'),
+    path(r'^posts/(?P<pk>[0-9]+)/$', MyModelView.as_view(), name='posts'),
     url(r'^post/(?P<pk>[0-9]+)/$', MyModelDownloadView.as_view(), name='post'),
     url(r'^write/$', WriteView.as_view(success_url="/posts/"), name='write')
-
 ]
 
 if settings.DEBUG:
