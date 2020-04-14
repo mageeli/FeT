c2d9f89282037b316f197ecf8fc202cf0f5ee6f4
mousa ageeli <mousaageeli@gmail.com>
2020-04-14 08:21:05 +0300

    django-heroku

--- cer/settings.py
+++ cer/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -128,3 +129,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+django_heroku.settings(locals())
